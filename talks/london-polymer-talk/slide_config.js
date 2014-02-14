bc10362ee565e59cd6a3faaadfc47cb34a161ede
Addy Osmani <addyosmani@gmail.com>
2014-02-14 11:27:55 +0000

    adding londonjs talk

---
+++ talks/london-polymer-talk/slide_config.js
@@ -0,0 +1,30 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Web Components <br> Can Do That?!',
+    subtitle: 'LondonJS Conf 2014',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: '/images/logos/chrome_logo.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro',
+      'Architects Daughter'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: '<a href="http://google.com/+Addy Osmani">Addy Osmani</a>',
+    company: 'LondonJS Conf',
+    gplus: 'http://google.com/+AddyOsmani',
+    twitter: '@addyosmani',
+    www: 'http://addyosmani.com',
+    github: 'http://github.com/addyosmani'
+  }]
+};
+
